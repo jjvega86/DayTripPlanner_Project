c1bd1cc218b657e918629fe073387d0c555ce4d8
JJ Vega <jjvega@jjs-macbook-pro.lan>
2020-11-30 12:23:13 -0500

    Added to do comments and mainMenu function to script.js

--- DayTripGenerator/script.js
+++ DayTripGenerator/script.js
@@ -1,9 +1,17 @@
 "use strict"
 //TO DO:
 // Create arrays for destination, restaurant, mode of transporation, and form of entertainment
+// Write functions to randomly generate trip features. 
+// Display the randomly generated trip to the user
+// Prompt for re-generate random features and confirm that the trip is complete
+// Display "Complete" in console to signify end of running script
 
 
 let destination = ["Virginia Beach", "Williamsburg", "Charlottsville", "Washington DC", "Richmond"];
 let restaurant = ["Enoteca Italian", "Gus's Fried Chicken", "Tim's Chinese Food", "The Table", "Stir Crazy"];
 let transporation = ["car", "bike", "train", "airplane"];
 let entertainment = ["live music", "zoo visit", "wine tasting", "brewery tour", "live sports"];
+
+function mainMenu(){
+    let userInput = prompt("Welcome to Day Trip Generator!");
+}
