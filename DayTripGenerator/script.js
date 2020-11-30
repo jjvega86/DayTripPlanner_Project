8bf0a132eba921ddcb9f3188a3cf8c8605b420e7
JJ Vega <jjvega@jjs-macbook-pro.lan>
2020-11-30 12:16:46 -0500

    "Added arrays to project for all day trip options"

--- DayTripGenerator/script.js
+++ DayTripGenerator/script.js
@@ -1,2 +1,9 @@
 "use strict"
+//TO DO:
+// Create arrays for destination, restaurant, mode of transporation, and form of entertainment
 
+
+let destination = ["Virginia Beach", "Williamsburg", "Charlottsville", "Washington DC", "Richmond"];
+let restaurant = ["Enoteca Italian", "Gus's Fried Chicken", "Tim's Chinese Food", "The Table", "Stir Crazy"];
+let transporation = ["car", "bike", "train", "airplane"];
+let entertainment = ["live music", "zoo visit", "wine tasting", "brewery tour", "live sports"];
