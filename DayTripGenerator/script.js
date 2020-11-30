80ec058bde20991e02b01fd636412eae67698aa4
JJ Vega <jjvega@jjs-macbook-pro.lan>
2020-11-30 13:36:14 -0500

    Finished refactor on buildDayTrip function

--- DayTripGenerator/script.js
+++ DayTripGenerator/script.js
@@ -14,9 +14,9 @@ let entertainmentForms = ["live music", "zoo visit", "wine tasting", "brewery to
 
 function RunDayTripGenerator(){
     // main "facade" function for application logic
-
+    var trip = buildDayTrip();
     alert("Welcome to Day Trip Generator!\nClick OK to see your trip!");
-    alert(buildDayTrip);
+    alert(trip);
 
     
     
@@ -29,19 +29,25 @@ function randomNumberGenerator(maxValue){
 }
 
 function generateRandomFeature(tripFeatures){
-    var randomIndex = randomNumberGenerator(tripFeatures.Length);
+    var randomIndex = randomNumberGenerator(tripFeatures.length);
 
     return tripFeatures[randomIndex];
 }
 
 function buildDayTrip(){
+
     let selectedDestination = generateRandomFeature(destinations);
     let selectedRestaurant = generateRandomFeature(restaurants);
     let selectedTransportation = generateRandomFeature(transporationModes);
     let selectedEntertainment = generateRandomFeature(entertainmentForms);
 
-    let entireTrip = selectedDestination + "" + selectedRestaurant + " " + selectedTransportation + " " + selectedEntertainment;
-
+    let entireTrip = "Destination: " + selectedDestination + "\n";
+    entireTrip += "Restaurant: " + selectedRestaurant + "\n";
+    entireTrip += "Transportation: " + selectedTransportation + "\n";
+    entireTrip += "Entertainment: " + selectedEntertainment + "\n";
+  
     return entireTrip;
 
 }
+
+RunDayTripGenerator();
